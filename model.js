5ead90c292f04fe48d069b40d34e755df1b18156
halvaradop <hernanvid123@gmail.com>
2023-02-17 21:20:23 -0500

    reduce code

--- model.js
+++ model.js
@@ -1,7 +1,11 @@
 import { choose, latest } from './data.js'
 
-const menu = document.querySelector('header > nav menu.w-90.absolute')
 const iconMenu = document.getElementById('iconMenu')
+const latestFragment = document.createDocumentFragment()
+const chooseFragment = document.createDocumentFragment()
+const menu = document.querySelector('header > nav menu.w-90.absolute')
+const latestTemplate = document.getElementById('latestTemplate').content
+const chooseTemplate = document.getElementById('chooseTemplate').content
 
 document.addEventListener('click', event => {
     if(event.target.matches('#iconMenu')) {
@@ -16,31 +20,33 @@ document.addEventListener('click', event => {
 })
 
 
-const latestFragment = document.createDocumentFragment()
-const chooseFragment = document.createDocumentFragment()
-const latestTemplate = document.getElementById('latestTemplate').content
-const chooseTemplate = document.getElementById('chooseTemplate').content
-
-console.log(latestTemplate)
-
-choose.forEach(choose => {
-    const cloneNode = document.importNode(chooseTemplate, true)
-    const { title, image, content } = choose
-    cloneNode.querySelector('img').setAttribute('src', image)
-    cloneNode.querySelector('h2').textContent = title
-    cloneNode.querySelector('p').textContent = content
-    chooseFragment.appendChild(cloneNode)
+choose.forEach((json, index) => {
+    const last = latest[index]
+    lookTemplatesHTML({ json, template: chooseTemplate, selectorImg: 'img', selectorTitle: 'h2', selectorContent: 'p', fragment: chooseFragment })
+    lookTemplatesHTML({ json: last, template: latestTemplate, selectorImg: 'img', selectorBy: '', selectorTitle: 'h3', selectorContent: 'h3 + p', selectorBy: 'p', fragment: latestFragment })
 })
 document.querySelector('main > section.my-15').append(chooseFragment)
-
-
-latest.forEach(latest => {
-    const cloneNode = document.importNode(latestTemplate, true)
-    const { by, title, content, image} = latest
-    cloneNode.querySelector('img').setAttribute('src', image)
-    cloneNode.querySelector('div > p').textContent = by
-    cloneNode.querySelector('div > h3').textContent = title
-    cloneNode.querySelector('h3 + p').textContent = content
-    latestFragment.append(cloneNode)
-})
 document.querySelector('section + section > section.flex').append(latestFragment)
+
+
+/**
+ * @typedef {Object} jsonData
+ * @property {Object} json
+ * @property {Element} template
+ * @property {String} selectorImg
+ * @property {String} selectorTitle
+ * @property {String} selectorContent
+ * @property {String} selectorBy
+ * @property {Element} fragment
+ * @param {jsonData}
+ */
+function lookTemplatesHTML({ json, template, selectorImg, selectorTitle, selectorContent, selectorBy, fragment }) {
+    const cloneNode = document.importNode(template, true)
+    const { by, title, content, image} = json
+    cloneNode.querySelector(selectorImg).setAttribute('src', image)
+    cloneNode.querySelector(selectorTitle).textContent = title
+    cloneNode.querySelector(selectorContent).textContent = content
+    selectorBy? cloneNode.querySelector(selectorBy).textContent = by : ''
+    fragment.append(cloneNode)
+}
+
